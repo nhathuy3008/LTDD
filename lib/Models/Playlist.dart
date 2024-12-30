1a40776f4fe51817807da3e4766e07651bbc6214
NhatHuy <nhathuy1237890@gmail.com>
2024-12-30 20:14:26 +0700

    update models playlist

--- lib/Models/Playlist.dart
+++ lib/Models/Playlist.dart
@@ -2,21 +2,23 @@ class Playlist {
   int id;
   String name;
   String accountId; // UUID của Account
+  String? image;
   List<int> songIds; // Danh sách ID bài hát
 
   Playlist({
     required this.id,
     required this.name,
+    required this.image,
     required this.accountId,
     required this.songIds,
   });
-
   factory Playlist.fromJson(Map<String, dynamic> json) {
     return Playlist(
       id: json['id'],
       name: json['name'],
-      accountId: json['accountId'],
-      songIds: List<int>.from(json['songIds']),
+      image: json['image'],
+      accountId: json['owner']['id'], // Lấy accountId từ owner
+      songIds: List<int>.from(json['songs'].map((song) => song['id'])), // Sửa thành 'songs'
     );
   }
 
@@ -24,6 +26,7 @@ class Playlist {
     return {
       'id': id,
       'name': name,
+      'image': image,
       'accountId': accountId,
       'songIds': songIds,
     };
