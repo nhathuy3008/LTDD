0ef28aa2800e16176dd2f2fcb1a931bc53b8bb6e
ThuHoai <thuhoai12042003@gmail.com>
2025-01-06 22:06:29 +0700

    update giao dien dang ki tai khoan

--- lib/Screens/register_page.dart
+++ lib/Screens/register_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../Services/account_service.dart';
 import '../Screens/verify_page.dart'; // Nhập trang VerifyScreen
+import 'login_page.dart'; // Thêm import cho LoginPage
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -28,22 +29,18 @@ class _RegisterPageState extends State<RegisterPage> {
     print('Dữ liệu gửi đến API: $accountData');
 
     try {
-      // Gọi phương thức createAccount và lưu kết quả
       final response = await _accountService.createAccount(accountData);
-      print('Phản hồi từ API: $response'); // In ra phản hồi
+      print('Phản hồi từ API: $response');
 
-      // Kiểm tra trường 'status' trong phản hồi
       if (response['status'] == 'thành công') {
         setState(() {
-          _message = response['message']; // Cập nhật thông báo
+          _message = response['message'];
         });
-        // Chuyển sang trang xác thực
         Navigator.push(
           context,
           MaterialPageRoute(builder: (context) => VerifyScreen(email: email)),
         );
       } else {
-        // Nếu không phải 'thành công', hiển thị thông báo lỗi
         setState(() {
           _message = response['message'] ?? 'Đăng ký thất bại!';
         });
@@ -59,32 +56,169 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Đăng Ký')),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _fullNameController,
-              decoration: InputDecoration(labelText: 'Họ Tên'),
-            ),
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(labelText: 'Email'),
-            ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Mật Khẩu'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _register,
-              child: Text('Đăng Ký'),
-            ),
-            SizedBox(height: 20),
-            Text(_message, style: TextStyle(color: Colors.red)),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Image.asset(
+                  "assets/images/vector-2.png",
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: MediaQuery.of(context).size.width * 0.4,
+                ),
+              ),
+              const SizedBox(height: 18),
+              const Text(
+                'Đăng Ký',
+                style: TextStyle(
+                  color: Color(0xFF755DC1),
+                  fontSize: 24,
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              const SizedBox(height: 30),
+              TextField(
+                controller: _fullNameController,
+                decoration: InputDecoration(
+                  labelText: 'Họ Tên',
+                  labelStyle: const TextStyle(
+                    color: Color(0xFF755DC1),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF837E93),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF9F7BFF),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: const TextStyle(
+                    color: Color(0xFF755DC1),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF837E93),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF9F7BFF),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Mật Khẩu',
+                  labelStyle: const TextStyle(
+                    color: Color(0xFF755DC1),
+                    fontSize: 14,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF837E93),
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(
+                      width: 1,
+                      color: Color(0xFF9F7BFF),
+                    ),
+                  ),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 20),
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 56,
+                  child: ElevatedButton(
+                    onPressed: _register,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color(0xFF9F7BFF),
+                    ),
+                    child: const Text(
+                      'Đăng Ký',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 15,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(_message, style: TextStyle(color: Colors.red)),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Đã có tài khoản?',
+                    style: TextStyle(
+                      color: Color(0xFF837E93),
+                      fontSize: 13,
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(width: 2.5),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context); // Quay lại trang đăng nhập
+                    },
+                    child: const Text(
+                      'Đăng nhập',
+                      style: TextStyle(
+                        color: Color(0xFF755DC1),
+                        fontSize: 13,
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
